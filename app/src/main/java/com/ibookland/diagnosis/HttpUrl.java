02d26bcc151532bf65259b300e50c8af6f14bfaa
sjkim <sjkimpro@hanmail.net>
2022-06-03 20:21:10 +0900

    WebViewTest

---
+++ app/src/main/java/com/ibookland/diagnosis/HttpUrl.java
@@ -0,0 +1,6 @@
+package com.ibookland.diagnosis;
+
+public class HttpUrl {
+    public static final String WEBURL = "http://gm.ileaderschool.com:16347";
+    //public static final String WEBURL = "http://www.naver.com";
+}
